d26b2de4f98e6f1d473d12210796acf86e17b7e3
khm0813@eduvation.co.kr <hyeongmin_kim@daekyo.co.kr>
2021-10-19 19:30:06 +0900

    # Chapter 2 ~ Chapter 4

--- com/practice/programmers/소수찾기.py
+++ com/practice/programmers/소수찾기.py
@@ -1,23 +1,27 @@
-from itertools import permutations
+from itertools import permutations # 조합
+import math
 
-
-def elatostenes(n):
-    chae = [False, False] + [True] * (n - 1)
-    prime = []
-    for i in range(2, n + 1):
-        if chae[i] == True:
-            prime.append(list(str(i)))
-            for j in range(2 * i, n + 1, i):
-                chae[j] = False
-    return prime
+def is_prime_number(n):
+    """소수판별 함수"""
+    if n == 0 or n == 1:  # 0,1 은 소수가 아님
+        return False
+    else:
+        for i in range(2, int(math.sqrt(n)) + 1):  # sqrt(n)까지만 for문을 돌면서 확인하면 된다.
+            if n % i == 0:  # 2~sqrt(num)까지 나누어 떨어지는 숫자가 있으면 소수가 아님
+                return False
+        return True
 
 
 def solution(numbers):
-    answer = 0
-    for i in range(len(numbers)+1):
-        print(list(permutations([0, 1, 1], i)))
+    answer = []
+    for i in range(1,len(numbers)+1):
+        arr = list(permutations(numbers, i))
+        for j in range(len(arr)):
+            num = int(''.join(map(str,arr[j])))
+            if is_prime_number(num):
+                answer.append(num)
 
-    return answer
+    return len(set(answer))
 
 
 s = ["17", "011"]
